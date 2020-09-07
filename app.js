220cdd4bc381a47c2d0c509fc07ac810a34e7e6e
Esraa Kamel <esraa.kamel1811@gmail.com>
2020-09-08 01:03:42 +0200

    Fix issues

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ let upload = multer({storage: storage});
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
