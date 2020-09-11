b73784e68fcaf5ff8c282165735c412493dbb566
Esraa Kamel <esraa.kamel1811@gmail.com>
2020-09-11 13:13:27 +0200

    change port declearation

--- app/app.js
+++ app/app.js
@@ -1074,6 +1074,5 @@ app.post("/sendEmailToAdmin" , (req, res) => {
 
 
 /////////////////////////////////////////////////////////////////////////////////
-PORT=process.env.PORT || 5000;
-app.listen(PORT, () => console.log('server listening on port 5000'));
+app.listen(process.env.PORT || 5000, () => console.log('server listening on port 5000'));
 
